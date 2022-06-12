412250765cb85df5b9b26dc667e1eea24f322565
Cascad <slashdrop@gmail.com>
2022-06-12 23:12:18 +0300

    set up project

---
+++ app/src/main/java/com/example/movier/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movier.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movier.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
